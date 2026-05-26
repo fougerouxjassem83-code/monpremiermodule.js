372af65c2ebd1019b5217048505611307937f18b
thevie <fougerouxjassem83@gmail.com>
2026-05-26 13:10:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 //**ICI JE REUNIRAIS TOUTS MES MODULES */
-
-
 //*ICI J'APPELLE LE FICHIER QUE JAI EXPORTER *//
 // quand je l'es appeler on a mis "
 //"./" c'est comme ca que je l'est appeleler */
@@ -10,9 +8,7 @@ import monHEURE from "./generateheure.js"
 console.log("mon heure : " ,
     monHEURE
 );
-
 //**ici j'afdfiche mon heur grace   a la DOM */
-
 const span = document.getElementById("heure");
 span.textContent = generateheure.monHeure();
 
