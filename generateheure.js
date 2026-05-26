420521504fa9a0d79b2e2cc8874177a3077b49a5
thevie <fougerouxjassem83@gmail.com>
2026-05-26 13:08:42 +0100

    Correct date object usage and log output
    Fix date instantiation and improve console log message.

--- generateheure.js
+++ generateheure.js
@@ -1,24 +1,17 @@
 //**
 // ** ICI JE Déclare la fonction qui renvoie l'heure *
 // /
-
-
 function monHeure (){
     let date = new date();
     let heure = date.getHours();
     console.log("coucou je passe a la télé à ",heure);
     return heure;
 }
-
-
-
-
 //**
 //  */ JE VAIS EXPORTER OU EXPOSER LA FONCTION monHEURE PARCE QUE JE VEUX LA FONCTION DEPUIS UN AUTREFICHIER
 
 export default{
     monHeure
 }
-
 
 console.log("main - mon heure",monHeure);
